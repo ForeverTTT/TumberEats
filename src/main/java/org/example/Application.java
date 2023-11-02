bcc71edb6542c07e6ec40f67999b4f2cb9afaa79
Michael Kormann <michael.kormann@sap.com>
2023-11-01 19:29:43 -0700

    refactor so the solution implementation is missing

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -3,8 +3,9 @@ package org.example;
 public class Application {
     public static void main(String[] args) {
         Delivery delivery1=new Delivery(5.6,new CalculateBike());
-        Delivery delivery2=new Delivery(11.73,new CalculateCar());
         System.out.println(delivery1.calculatePrice());
-        System.out.println(delivery2.calculatePrice());
+        //Todo uncomment to test your implementation
+        //Delivery delivery2=new Delivery(11.73,new CalculateCar());
+        //System.out.println(delivery2.calculatePrice());
     }
 }
--- src/main/java/org/example/CalculateCar.java
+++ src/main/java/org/example/CalculateCar.java
@@ -1,8 +1,5 @@
 package org.example;
 
-public class CalculateCar implements PriceStrategy {
-    @Override
-    public double calculatePrice(double distance) {
-        return 3.2 + distance / 2.6;
-    }
+public class CalculateCar {
+  //Todo implement class
 }
